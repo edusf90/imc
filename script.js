d0b4318451e54906d71aea3dcf76ca03c59f7f6c
Eduardo Santana <edusf0@hotmail.com>
2022-07-11 16:26:40 -0300

    meu imc

---
+++ script.js
@@ -0,0 +1,57 @@
+function enviar() {
+  let nomePego = document.getElementById('nome').value
+  let pesoPego = document.getElementById('peso').value
+  let alturaPego = document.getElementById('altura').value
+
+  let formPego = document.getElementById('formResultado')
+  formPego.classList.add('resultado')
+
+  function aviso() {
+    let aviso = document.getElementById('avisoColocado')
+    aviso.classList.add('error')
+  }
+
+  if (nomePego == '') {
+    // Se o nome estiver vazio
+    aviso()
+    document.getElementById('avisoColocado').innerHTML = 'Nome vazio'
+  } else if (pesoPego == '') {
+    // Se o Peso estiver vazio
+    aviso()
+    document.getElementById('avisoColocado').innerHTML = 'Peso vazio'
+  } else if (alturaPego == '') {
+    // alert('Altura vazia')
+    aviso()
+    document.getElementById('avisoColocado').innerHTML = 'Altura vazio'
+  } else {
+    document.getElementById('avisoColocado').innerHTML = ''
+
+    let resultadoIMC = pesoPego / (alturaPego * alturaPego)
+    resultadoIMC = resultadoIMC.toFixed(2)
+
+    document.getElementById('nomeColocado').innerHTML = 'Nome: ' + nomePego
+    document.getElementById('pesoColocado').innerHTML =
+      'Peso: ' + pesoPego + ' kilos'
+    document.getElementById('alturaColocado').innerHTML =
+      'Altura: ' + alturaPego
+    document.getElementById('imcColocado').innerHTML = 'IMC: ' + resultadoIMC
+
+    let statusIMC
+    if (resultadoIMC < 18.5) {
+      statusIMC = 'Abaixo do Peso'
+    } else if (resultadoIMC >= 18.5 && resultadoIMC <= 24.999) {
+      statusIMC = 'Peso Normal'
+    } else if (resultadoIMC >= 25 && resultadoIMC <= 29.999) {
+      statusIMC = 'Sobrepeso'
+    } else if (resultadoIMC >= 30 && resultadoIMC <= 34.999) {
+      statusIMC = 'Obesidade Grau 1'
+    } else if (resultadoIMC >= 35 && resultadoIMC <= 39.999) {
+      statusIMC = 'Obesidade Grau 2'
+    } else if (resultadoIMC >= 40) {
+      statusIMC = 'Obesidade Grau 3 ou Mórbida'
+    }
+
+    document.getElementById('statusColocado').innerHTML =
+      'Seu Status é: ' + statusIMC
+  }
+}
---
+++ testeimc.js
@@ -0,0 +1,35 @@
+function AbaixoPeso() {
+  document.querySelector("[id='nome']").value = 'Messias Dias Fartaria'
+  document.querySelector("[id='peso']").value = 20
+  document.querySelector("[id='altura']").value = 1.53
+}
+
+function Normal() {
+  document.querySelector("[id='nome']").value = 'Estefânia Rosmaninho Caetano'
+  document.querySelector("[id='peso']").value = 60
+  document.querySelector("[id='altura']").value = 1.65
+}
+
+function Sobrepeso() {
+  document.querySelector("[id='nome']").value = 'Caetano Novais Moutinho'
+  document.querySelector("[id='peso']").value = 80
+  document.querySelector("[id='altura']").value = 1.75
+}
+
+function Obesidade1() {
+  document.querySelector("[id='nome']").value = 'Clara Leme Jordão'
+  document.querySelector("[id='peso']").value = 110
+  document.querySelector("[id='altura']").value = 1.85
+}
+
+function Obesidade2() {
+  document.querySelector("[id='nome']").value = 'Eva Antas Noite'
+  document.querySelector("[id='peso']").value = 110
+  document.querySelector("[id='altura']").value = 1.72
+}
+
+function Obesidade3() {
+  document.querySelector("[id='nome']").value = 'Alissa Rufino Roriz'
+  document.querySelector("[id='peso']").value = 100
+  document.querySelector("[id='altura']").value = 1.52
+}
